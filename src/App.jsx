898cfb6bf8e4b5463d3f27e487ba9534392cacf3
ikoindra <ikoindra91@gmail.com>
2024-11-22 20:22:35 +0700

    shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Button } from "@/components/ui/button";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function Home() {
   return (
-    <>
-      <div class="bg-blue-500 text-white p-4 rounded-lg">
-        Tailwind is working!
-      </div>
-    </>
+    <div>
+      <Button>Click me</Button>
+    </div>
   );
 }
-
-export default App;
