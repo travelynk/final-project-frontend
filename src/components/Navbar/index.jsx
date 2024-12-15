bc681d103842e1d8abe9f5a9e8821add9063f631
Quls <Yudriqul@0803@gmail.com>
2024-12-15 14:58:31 +0700

    resoleve conflict

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -14,11 +14,6 @@ const NavigationBar = () => {
 
   return (
     <header className="bg-white shadow-md py-3 border-b-2">
-      <div className="container mx-auto flex flex-wrap items-center justify-between">
-        {/* Logo Section */}
-        <div className="flex items-center space-x-2 ms-5">
-          <Link to="/">
-            <img src="/binar.svg" alt="TraveLynk" className="w-24 h-8" />
       <div className="container mx-auto flex flex-col md:flex-row md:justify-between md:px-4">
         <div className="hidden md:flex items-center space-x-2 mb-3 md:mb-0">
           <Link as={Link} to="/">
