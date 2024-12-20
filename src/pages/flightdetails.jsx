9e3a9f47ac11177db6d4b97def1cebf2761edc26
Quls <Yudriqul@0803@gmail.com>
2024-12-20 14:44:01 +0700

    resolve bug UI

--- src/pages/flightdetails.jsx
+++ src/pages/flightdetails.jsx
@@ -581,7 +581,7 @@ export default function FlightDetail({
                     {isFetchingVouchers ? "Loading..." : "Cek"}
                   </button>
                 </PopoverTrigger>
-                <PopoverContent className="bg-white shadow-md rounded-lg p-4 w-64">
+                <PopoverContent className="bg-white shadow-md rounded-lg p-4 w-64 dark:text-black">
                   <h3 className="font-bold text-lg mb-2">Daftar Voucher</h3>
                   <ul className="space-y-2">
                     {vouchers.map((voucher) => (
